8890e78a98c0e0a4cc34ba31f8b9e11431d6d7c2
Jenil Desai <jenildev91@gmail.com>
2024-12-04 17:04:59 +0530

    v1.1.0
    ➕ Added Password Generation Logic.

--- App.tsx
+++ App.tsx
@@ -7,12 +7,41 @@ export default function App() {
 
   const [lowerCase, setLowerCase] = useState(true);
   const [upperCase, setUpperCase] = useState(true);
-  const [numbers, setNumber] = useState(true);
-  const [symbols, setSymbols] = useState(true);
+  const [numbers, setNumber] = useState(false);
+  const [symbols, setSymbols] = useState(false);
 
-  const generatePasswordString = (passwordLenght: number) => {};
-  const createPassword = (characters: string, passwordLenght: number) => {};
-  const resetPasswordState = () => {};
+  const generatePasswordString = (passwordLenght: number) => {
+    let characterList = '';
+    const upperCaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const lowerCaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.toLowerCase();
+    const digitsChars = '0123456789';
+    const specialChars = `!@#$%^&*()_+{}|:"<>?,./;'[]\``;
+
+    if (upperCase) characterList += upperCaseChars;
+    if (lowerCase) characterList += lowerCaseChars;
+    if (numbers) characterList += digitsChars;
+    if (symbols) characterList += specialChars;
+
+    const passwordResult = createPassword(characterList, passwordLenght);
+    setPassword(passwordResult);
+    setIsPassGenerated(true);
+  };
+  const createPassword = (characters: string, passwordLenght: number) => {
+    let result = '';
+    for (let index = 0; index < passwordLenght; index++) {
+      const characterIndex = Math.round(Math.random() * characters.length);
+      result += characters.charAt(characterIndex);
+    }
+    return result;
+  };
+  const resetPasswordState = () => {
+    setPassword('');
+    setIsPassGenerated(false);
+    setLowerCase(true);
+    setUpperCase(true);
+    setNumber(false);
+    setSymbols(false);
+  };
 
   return (
     <View>
